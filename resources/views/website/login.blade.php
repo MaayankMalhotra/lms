5f7bd6ca92adcf5e50d37c82f82ac0de6de61edb
maayank <maayank.malhotra@cracode.com>
2025-06-04 11:59:01 +0530

    push

--- resources/views/website/login.blade.php
+++ resources/views/website/login.blade.php
@@ -77,7 +77,12 @@
           </svg>
         </a>
       </div>
-
+      <!-- Success Message -->
+      @if (session('success'))
+        <div class="bg-green-500 text-white p-4 rounded-lg mb-6">
+          {{ session('success') }}
+        </div>
+      @endif
       <!-- Error Messages -->
       @if (session('error'))
         <div class="bg-red-500 text-white p-4 rounded-lg mb-6">
